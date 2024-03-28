b0e1756f54873b412203cb0082773a59a73c10b1
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-03-28 15:02:41 +0530

    changer: remove console

--- helper/response.js
+++ helper/response.js
@@ -2,7 +2,6 @@ const { GeneralResponse } = require('../utils/response');
 const { StatusCodes } = require('http-status-codes');
 
 const handleResponse = (response, req, res, next) => {
-  console.log('response',response);
   if (response instanceof GeneralResponse) {
 
     return res.status(StatusCodes.OK).json({
